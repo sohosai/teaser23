837a38f2be86bb0178d1b457d2dea32816208923
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-06-22 19:10:38 +0900

    feat: add i18n utils

---
+++ src/utils/ui.ts
@@ -0,0 +1,14 @@
+import ja from "../content/locales/ja.json";
+import en from "../content/locales/en.json";
+
+export const languages = {
+  ja: "日本語",
+  en: "English",
+};
+
+export const defaultLang = "ja";
+
+export const ui = {
+  ja: ja,
+  en: en,
+} as const;
