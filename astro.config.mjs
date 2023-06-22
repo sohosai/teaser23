8c26bfbc06489c40ce00c60ff1894253952ba560
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-06-22 18:15:57 +0900

    try astro-i18n-aut

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,30 @@
 import { defineConfig } from "astro/config";
+import i18n from "astro-i18n-aut";
+import sitemap from "@astrojs/sitemap";
+
+const defaultLocale = "ja";
+const locales = {
+  ja: "ja-JP",
+  en: "en-US",
+};
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://sohosai.com",
+  trailingSlash: "always",
+  build: {
+    format: "directory",
+  },
+  integrations: [
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+    }),
+  ],
+});
