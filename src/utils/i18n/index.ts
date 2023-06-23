30bc71ae6f6eb908e43c5517c11b6f4724a0af8e
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-06-24 01:19:57 +0900

    feat: add astro-i18n like feature

--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -1,36 +1,27 @@
-import LocaleJa from "../../content/locales/ja/translation.json";
-import LocaleEn from "../../content/locales/en/translation.json";
-
 import { i18n as i18nAut } from "astro-i18n-aut";
 import i18nPlus from "./core/integration";
+import type { i18nProps } from "./types";
 
-export default function i18n(defaultLocale: string, locales: Record<string, string>) {
+export default function i18n({
+  include = ["pages/**/*"],
+  exclude = ["pages/api/**/*"],
+  locales,
+  defaultLocale,
+}: i18nProps) {
   return [
     i18nAut({
+      include,
+      exclude,
       locales,
       defaultLocale,
     }),
-    i18nPlus(defaultLocale),
+    i18nPlus({
+      locales,
+      defaultLocale,
+    }),
   ];
 }
 
-export const defaultLang = "ja";
-
-const ui = {
-  ja: LocaleJa,
-  en: LocaleEn,
-} as const;
-
-export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split("/");
-  if (lang in ui) return lang as keyof typeof ui;
-  return defaultLang;
-}
-
-export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
-  };
-}
-
 export { default as i18nAdapter } from "./core/state";
+
+export { t } from "./core/translation";
--- src/utils/i18n/types/index.ts
+++ src/utils/i18n/types/index.ts
@@ -1,3 +1,6 @@
 export type i18nAdapterConfig = {
   defaultLocale: string;
+  supportedLocales: string[];
 };
+
+export type { I18nParameters as i18nProps } from "astro-i18n-aut";
