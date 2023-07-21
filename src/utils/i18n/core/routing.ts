056ed80a3e120b643a50226c0a6d429f7afeecfe
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-07-21 21:36:26 +0900

    feat: impl #13

---
+++ src/utils/i18n/core/routing.ts
@@ -0,0 +1,15 @@
+import i18nAdapter from "./state";
+
+export function l(route: string, locale = i18nAdapter.locale, routeLangCode = ""): string {
+  const { locales, defaultLocale } = i18nAdapter;
+  const segments = route.replace(/^\/+|\/+$/g, "").split("/");
+  if (locales.includes(segments[0])) {
+    segments.shift();
+  }
+  const inputLangCode = routeLangCode || locale || defaultLocale;
+  let result = segments.join("/");
+  if (inputLangCode !== defaultLocale) {
+    result = `${inputLangCode}/${result}`;
+  }
+  return "/" + result + "/";
+}
