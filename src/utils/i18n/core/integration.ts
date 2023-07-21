119368bce5ca2361ce0a8a0ef1e5de53a23ce2a1
OKAMOTO Shigehiro <primary@goahi.live>
2023-07-22 01:20:54 +0900

    fix: add a integration to rename /en/404/index.html with /en/404.html

--- src/utils/i18n/core/integration.ts
+++ src/utils/i18n/core/integration.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 import type { AstroIntegration } from "astro";
 import type { i18nProps } from "../types";
 import type { i18nAdapterConfig } from "../types";
@@ -27,6 +29,34 @@ export default function i18nPlus({ locales, defaultLocale }: i18nProps): AstroIn
             `i18nAdapter.internals().init(${stringifiedConfig}, translates);`,
           ].join("")
         );
+      },
+
+      "astro:build:done": async ({ dir }) => {
+        const config: i18nAdapterConfig = {
+          defaultLocale: defaultLocale,
+          supportedLocales: Object.keys(locales),
+        };
+
+        config.supportedLocales
+          .filter((i) => i !== defaultLocale)
+          .forEach((locale) => {
+            const locale_dir = dir.pathname + locale;
+            fs.rename(`${locale_dir}/404/index.html`, `${locale_dir}/404.html`, (err) => {
+              if (err) {
+                console.log("Failed to rename path of 404 files");
+                throw err;
+              }
+            });
+
+            fs.rmdir(`${locale_dir}/404`, (err) => {
+              if (err) {
+                console.log("Failed to rename path of 404 files");
+                throw err;
+              }
+            });
+          });
+
+        //await Promise.all(config.supportedLocales.forEach);
       },
     },
   };
