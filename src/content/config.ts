97164556e87158a9c0a207bfa92f79faa1030c05
OKAMOTO Shigehiro <primary@goahi.live>
2023-09-21 10:21:02 +0900

    feat: add news using a content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,15 @@ const locales = defineCollection({
   type: "data",
 });
 
+const news = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    publishedAt: z.date(),
+  }),
+});
+
 export const collections = {
   locales: locales,
+  news: news,
 };
