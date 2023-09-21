666946409bcf12fbe6f11f120ffe6fed26ac44b6
OKAMOTO Shigehiro <primary@goahi.live>
2023-09-21 10:23:41 +0900

    Revert "feat: add news using a content collection"
    This reverts commit 97164556e87158a9c0a207bfa92f79faa1030c05.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,15 +4,6 @@ const locales = defineCollection({
   type: "data",
 });
 
-const news = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    publishedAt: z.date(),
-  }),
-});
-
 export const collections = {
   locales: locales,
-  news: news,
 };
