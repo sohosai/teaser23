140662b79a06e36e68900589154541f22fb4bc1b
OKAMOTO Shigehiro <primary@goahi.live>
2023-10-09 10:36:39 +0900

    Fix: Optimize head

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,12 @@
 import { t } from "src/utils/i18n";
 
 import type { YYYYMMDD } from "src/types/date";
-export const DEFAULT_OGP_IMAGE = "ogp.png";
 export const START_DATE: YYYYMMDD = "2023-11-04";
 export const END_DATE: YYYYMMDD = "2023-11-05";
 export const DEFAULT_TIMEZONE = "Asia/Tokyo";
+
+export const TWITTER_ID = "@sohosai";
+export const DEFAULT_OGP_IMAGE = "ogp.png";
 
 const keys = [
   "base.雙峰祭",
