bcd6ab952a9e4cdb5dca7cd38c22230f19c7b897
OKAMOTO Shigehiro <primary@oka4shi.dev>
2024-12-19 18:43:04 +0900

    2025年の情報に更新

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 import { t } from "src/utils/i18n";
 
 import type { YYYYMMDD } from "src/types/date";
-export const ZENYASAI_DATE: YYYYMMDD = "2024-11-02";
-export const START_DATE: YYYYMMDD = "2024-11-03";
-export const END_DATE: YYYYMMDD = "2024-11-04";
+export const ZENYASAI_DATE: YYYYMMDD = "2025-11-01";
+export const START_DATE: YYYYMMDD = "2025-11-02";
+export const END_DATE: YYYYMMDD = "2025-11-03";
 export const DEFAULT_TIMEZONE = "Asia/Tokyo";
 
 export const TWITTER_ID = "@sohosai";
